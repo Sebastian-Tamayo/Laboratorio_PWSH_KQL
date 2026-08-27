82956d3d58a49c6669e28f60b148292392aa9322
Sebas-Dev <153756322+Sebastian-Tamayo@users.noreply.github.com>
2026-08-27 08:22:51 +0200

    Añadidos ejercicios

--- powershell_KQL/PWSH_KQL/Scripts/BLOQUE-03-PowerShell-Azure KQL/resource-graph.ps1
+++ powershell_KQL/PWSH_KQL/Scripts/BLOQUE-03-PowerShell-Azure KQL/resource-graph.ps1
@@ -1,13 +1,20 @@
-# 1. Definimos la query KQL 
+#  Prerrequisito: Autenticación contra tu tenant de Azure
+# En un entorno automatizado (Runbook/Pipeline), esto se haría mediante Managed Identity.
+# En tu consola local, me pide que me autentique con mi cuenta de Azure.
+Connect-AzAccount
+
+#  Definición de la Query KQL 
 $queryKQL = @"
 Resources
+#  filtramos por tipo de recurso (Storage Account) y por región (France Central)
 | where type =~ 'microsoft.storage/storageaccounts'
 | where location == 'francecentral'
 | project name, location, sku.name
 "@
 
-# 2. Ejecutamos la consulta contra Azure Resource Graph
-$resultadoStorage = Search-AzGraph -Query $queryKQL
+# 4. Lanzamiento del Cmdlet
+# Buenas prácticas: Usa -First para limitar resultados 
+$resultados = Search-AzGraph -Query $queryKQL -First 1000
 
-# 3. Mostramos el resultado por pantalla
-$resultadoStorage
+# Mostrar en tabla para lectura visual rápida:
+$resultados | Format-Table -AutoSize
